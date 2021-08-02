75374f0c98a1c5565aadbedcee9921d64d60f397
Davide Cristoni <davide@cristoni.it>
2021-08-02 20:43:08 +0200

    aggiunta di bootstrap e primo tentativo di navbar

---
+++ src/Navbar.js
@@ -0,0 +1,15 @@
+import React from "react";
+const nav = () => {
+    return ( 
+        <nav className="navbar bg-light">
+            <div className="container">
+                <a href="#" className="navbar-brand">
+                    CATER
+                </a>
+                
+            </div>
+        </nav>
+     );
+}
+ 
+export default nav;
