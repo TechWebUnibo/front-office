76375cfee3a71766b504c59cc03a7142b49c3360
elements72 <antonio.lopez.8523@gmail.com>
2021-10-15 11:54:51 +0200

    Fixed yarn/npm problem, now jsonwebtoken works

--- src/Products.js
+++ src/Products.js
@@ -1,10 +1,11 @@
 import React from "react";
 import { Container} from "react-bootstrap";
-import logo from "../assets/logo.png";
 import ProductCard from "./components/ProductCard";
 
 const Products = () => { 
   
+  const logo = '/box'
+
     const products= [{title: "Gruppo 1", price:245.68, img:{logo}},
     {title: "Gruppo 2", price:285.68, img:{logo}},
     {title: "Gruppo 3", price:345.68, img:{logo}},
