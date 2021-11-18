f38b719be82a11e0f9a10532e07f0a7055c751b1
Antonio Lopez <antonio.lopez2@studio.unibo.it>
2021-11-18 22:46:24 +0100

    Added basic style to invoicePage

--- src/components/InvoicePage.js
+++ src/components/InvoicePage.js
@@ -1,8 +1,10 @@
 import React, {useEffect, useRef, useState} from "react"
-import {Button, Container, ListGroup, Spinner} from "react-bootstrap";
+import {Button, Container, Spinner, Row, Col, Alert} from "react-bootstrap";
 import {useParams} from "react-router-dom";
 import {useReactToPrint} from 'react-to-print';
 import {getInvoices, getUser} from "../utility/apiLibrary";
+import '../style/common.css';
+import '../style/invoicePage.css';
 
 const InvoicePagePrintable = React.forwardRef(({prop}, ref) => {
     const {id} = useParams();
@@ -19,7 +21,7 @@ const InvoicePagePrintable = React.forwardRef(({prop}, ref) => {
             }
         }
         fetchInvoices()
-    }, [])
+    }, [id])
 
     return (
         <div ref={ref} className="container">
@@ -33,31 +35,63 @@ const InvoicePagePrintable = React.forwardRef(({prop}, ref) => {
             {!isPending &&
             (
                 <div>
-                    <h2 className="display-2">Fattura #{invoice._id}</h2>
-                    <p>Codice cliente: {invoice.customer}
-                        <br/>
-                    Codice impiegato: {invoice.employee}</p>
-                    <h3 className="display-4">Prodotti</h3>
-                    <ul>
+                    <Row>
+                        <Col md={4}>
+                            <img src="//site202118.tw.cs.unibo.it/img/logo.eda7be37.png" alt="CATER" style={{ height: "4em" }}></img>
+                        </Col>
+                        <Col md={8} className="text-end mt-4">
+                            <h3 className="small">Fattura: #{invoice._id}</h3>
+                            <h3 className="small">Ordine: #{invoice.rent}</h3>
+                            <h3 className="small">Codice cliente: {invoice.customer}</h3>
+                            <h3 className="small">Codice impiegato: {invoice.employee}</h3>
+                        </Col>
+                    </Row>
+                    <h2 className="mt-4">Noleggio:</h2>
+                    <Row className="mt-2">
+                        <Col>
+                            <h4>Data di inizio: {invoice.start.split('T')[0]}</h4>
+                        </Col>
+                        <Col>
+                            <h4>Data di fine: {invoice.end.split('T')[0]}</h4>
+                        </Col>
+                    </Row>
+
+                    <hr />
+
+                    <Row>
+                        <Col>
+                            <h3>
+                                Nome
+                            </h3>
+                        </Col>
+                        <Col>
+                            <h3>
+                                Stato di riconsegna
+                            </h3>
+                        </Col>
+                    </Row>
+
                     {Object.keys(invoice.products).map((prod) => {
                         return (
-                            <li key={prod}>
-                                <div>
-                                    <h4>Prodotto: {prod} </h4>
-                                </div>
-                            </li>
+                            <Row key={prod}>
+                                <Col>
+                                    <h4>{prod}</h4>
+                                </Col>
+                                <Col>
+                                    <h4>{invoice.products[prod].condition}</h4>
+                                </Col>
+                            </Row>
                         )
                     })}
-                    </ul>
-                    <p>Prezzo € {invoice.price}</p>
-                    <p>Data di inizio: {invoice.start.split('T')[0]}
-                        <br/>
-                        Data di fine: {invoice.end.split('T')[0]}
-                    </p>
+                    <Row className='mt-4'>
+                        <Col md={{offset: 10 }}>
+                            <Alert variant={'secondary'}>
+                                Totale: {invoice.price}
+                            </Alert>                        
+                        </Col>
+                    </Row>
                 </div>
-            )
-            }
-
+            )}
             {(!isPending && invoice === {}) &&
             (<h3> Fattura non disponibile</h3>)
             }
@@ -74,7 +108,7 @@ function InvoicePage() {
     });
 
     return (
-        <Container>
+        <Container className="container mt-4 content">
             <InvoicePagePrintable ref={componentRef}/>
             <Button onClick={handlePrint}>Stampa</Button>
         </Container>
