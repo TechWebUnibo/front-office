904467093abcb503b7610337c00dd6ec18656765
Davide Cristoni <davide@cristoni.it>
2021-12-19 12:13:16 +0100

    rounded img su dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -75,7 +75,7 @@ const Dashboard = () => {
                                         <Card.Body>
                                             <Row>
                                                 <Col xs={4} className="align-self-center">
-                                                <Image fluid alt='Avatar del profilo' src={card.img} {...card.link === '/profile'  && 'roundedCircle'} />
+                                                <Image fluid alt='Avatar del profilo' src={card.img} className={card.link === '/profile' ? 'roundedCircle' : ''} />
                                                     {/*<Card.Img variant="top" src={card.img} roundedCircle />*/}
                                                 </Col>
                                                 <Col xs={8}>
