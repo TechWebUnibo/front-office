ab2693ad295f7bdcb04bc111597a73bf62d4bd8a
Davide Cristoni <davide@cristoni.it>
2021-12-19 11:37:02 +0100

    aggiunto avatar nella dashboard

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,17 +1,18 @@
-import { Row, Col, Card, Container } from 'react-bootstrap'
+import { Row, Col, Card, Container, Spinner } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import { getCustomer, getUser } from '../utility/apiLibrary'
 
 import seo from '../utility/dynamicPageTitle'
 
-const Dashboard = () =>{
+const Dashboard = () => {
 
     //Dynamic page title
-    seo({title : 'Profilo | Cater', metaDescription : 'Il tuo profilo'})
+    seo({ title: 'Profilo | Cater', metaDescription: 'Il tuo profilo' })
 
     const [customer, setCustomer] = useState({})
-    
+    const [isPending, setIsPending] = useState(true);
+
     const cards = [
         {
             title: 'Informazioni personali',
@@ -40,47 +41,59 @@ const Dashboard = () =>{
     ]
 
     useEffect(() => {
+        setIsPending(true)
         const loadProfile = async function () {
             let res = await getCustomer(await getUser())
-            if(res){
-                if(res.avatar) {
-                    cards[0].img = customer.avatar
+            if (res) {
+                if (res.avatar) {
+                    cards[0].img = res.avatar
                 }
                 setCustomer(res)
             }
+            setIsPending(false)
         }
-       loadProfile() 
+        loadProfile()
     }, [])
 
-    return(
-        <Container>
-            <h2 className="title">Welcome {customer.username}</h2>
-            <Row xs={1} md={2} className="g-4 my-2">
-                {cards.map((card) => (
-                    <Col key={card.title}>
-                        <Link to={card.link}>
-                            <Card>
-                            <Card.Body>
-                                <Row>
-                                    <Col xs={4} className="align-self-center">
-                                        <Card.Img variant="top" src={card.img} roundedCircle/>
-                                    </Col>
-                                    <Col xs={8}>
-                                        
-                                            <Card.Title>{card.title}</Card.Title>
-                                            <Card.Text>
-                                                {card.text}
-                                            </Card.Text>
-                                    </Col>
-                                </Row>
-                                </Card.Body>
-                            </Card>
-                        </Link>
-                    </Col>
-            ))}
-        </Row>
-        </Container>
-        
+    cards[0].img = customer.avatar
+
+    return (
+        <>
+            {isPending && (
+                <Container>
+                    <Spinner animation="border" size="m" />
+                </Container>
+            )}
+            {!isPending && (
+                <Container>
+                    <h2 className="title">Welcome {customer.username}</h2>
+                    <Row xs={1} md={2} className="g-4 my-2">
+                        {cards.map((card) => (
+                            <Col key={card.title}>
+                                <Link to={card.link}>
+                                    <Card>
+                                        <Card.Body>
+                                            <Row>
+                                                <Col xs={4} className="align-self-center">
+                                                    <Card.Img variant="top" src={card.img} roundedCircle />
+                                                </Col>
+                                                <Col xs={8}>
+
+                                                    <Card.Title>{card.title}</Card.Title>
+                                                    <Card.Text>
+                                                        {card.text}
+                                                    </Card.Text>
+                                                </Col>
+                                            </Row>
+                                        </Card.Body>
+                                    </Card>
+                                </Link>
+                            </Col>
+                        ))}
+                    </Row>
+                </Container>
+            )}
+        </>
     )
 }
 
